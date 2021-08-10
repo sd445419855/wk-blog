99b18d6bfdf761134a799e9343ccd47a2f7fda4c
Kai Gao <445419855@qq.com>
2021-08-10 21:04:33 +0800

    修改项目目录结构，编译目录名称改为'build'

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-### reco_luan's blog [website](https://www.recoluan.com)
-# 午后南杂博客主题  在主题3选一时，选择的 afternoon-grocery 午后南杂
-
 # kw-blog
 kai and wan's IT technology and life blog
+
+# reco_luan's blog [website](https://www.recoluan.com)
+午后南杂博客主题  在主题3选一时，选择的 afternoon-grocery 午后南杂
+
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,9 +2,9 @@ const themeConfig = require('./config/theme/')
 
 module.exports = {
   title: "午后南杂",
-  base: '/public/',      // 解决 build 后的样式失效问题，末尾需带 /
+  base: '/build/',      // 解决 build 后的样式失效问题，末尾需带 /
   description: 'Enjoy when you can, and endure when you must.',
-  dest: 'public',
+  dest: 'build',   // public
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
