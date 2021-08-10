89feafebdc3f8b8b0deb2bc7b735956def47101a
Kai Gao <445419855@qq.com>
2021-08-10 20:40:49 +0800

    初始博客，模板为午后南杂

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+### reco_luan's blog [website](https://www.recoluan.com)
+# 午后南杂博客主题  在主题3选一时，选择的 afternoon-grocery 午后南杂
+
 # kw-blog
 kai and wan's IT technology and life blog
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+const themeConfig = require('./config/theme/')
+
+module.exports = {
+  title: "午后南杂",
+  base: '/public/',      // 解决 build 后的样式失效问题，末尾需带 /
+  description: 'Enjoy when you can, and endure when you must.',
+  dest: 'public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  theme: 'reco',
+  themeConfig,
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: ['@vuepress/medium-zoom', 'flowchart'] 
+}  
