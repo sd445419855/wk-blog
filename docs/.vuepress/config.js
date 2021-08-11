9d4145337012ac5556eeed1378341c1a4a1bc3dc
Kai Gao <445419855@qq.com>
2021-08-11 23:17:09 +0800

    style:修改banner大小标题内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,56 @@
+/**
+ *  配置文件
+ */ 
+
+// 午后牛腩主题配置，没有他，就变成最原始的博客配置了。
 const themeConfig = require('./config/theme/')
 
 module.exports = {
-  title: "午后南杂",
+  title: "Life is Wanwan",
   base: '/',      // 解决 build 后的样式失效问题，末尾需带 /
-  description: 'Enjoy when you can, and endure when you must.',
+  description: '耐得住寂寞，才能守得住繁华，不忘初心，方得始终',    // banner 的小标题
   dest: 'build',   // public
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    [
+      'link', 
+      { 
+        rel: 'icon', href: '/favicon.ico' 
+      }
+    ],
+    [
+      'meta', 
+      { 
+        name: 'viewport', 
+        content: 'width=device-width,initial-scale=1,user-scalable=no' 
+      }
+    ]
   ],
+  // 主题 reco
   theme: 'reco',
-  themeConfig,
+  // 主题配置
+  themeConfig: {
+    // 模板自带的午后牛腩主题配置
+    ...themeConfig
+    // 类型
+    // type: 'blog',
+    // 头像
+    // authorAvatar: 'https://avatars.githubusercontent.com/u/58387070?s=400&u=b1794932734cf193cb7139dbeddef7f8fab4ff46&v=4',
+    // 404 腾讯公益
+    // noFoundPageByTencent: true,
+    // 博客配置
+    // blogConfig: {
+    //   // 导航 - category
+    //   category: {
+    //     location: 2,     // 在导航栏菜单中所占的位置，默认2
+    //     text: 'My Category' // 默认文案 “分类”
+    //   },
+    //   // 导航 - tag
+    //   tag: {
+    //     location: 3,     // 在导航栏菜单中所占的位置，默认3
+    //     text: 'My Tag'      // 默认文案 “标签”
+    //   }
+    // }
+  },
   markdown: {
     lineNumbers: true
   },
--- docs/README.md
+++ docs/README.md
@@ -1,27 +1,44 @@
 ---
+# 配置文件
+
 home: true
-bgImage: '/banner.jpg'
-bgImageStyle: {
+
+# background image
+bgImage: '/banner.jpg'    # 背景图，横幅
+bgImageStyle: {           # 背景图样式
   height: '450px'
 }
-faceImage: '/head.png'
-heroImage: /hero_write.png
+
+# hero text banner 的大标题
+heroText: "越努力，越幸运"        # 越努力，越幸运    
+
+# 头像，默认值 '/head.png'
+faceImage: "https://avatars.githubusercontent.com/u/58387070?s=400&u=b1794932734cf193cb7139dbeddef7f8fab4ff46&v=4"    
+
+# hero image  # config.js 中的午后牛杂 themeConfig 会让该项失效
+heroImage: /hero_write.png      # /hero_write.png
 heroImageStyle: {
-  maxHeight: '200px',
+  maxHeight: '400px',   # 200px
+  # width: '100%',        # 新增
   display: block,
   margin: '6rem auto 1.5rem',
   borderRadius: '50%',
   boxShadow: '0 5px 18px rgba(0,0,0,0.2)'
+  # maxWidth: '600px',
+  # width: '100%',
+  # display: block,
+  # margin: '9rem auto 2rem',
+  # borderRadius: '1rem',
 }
-isShowTitleInHome: false
-actionText: About Me
+isShowTitleInHome: false          # 什么作用？
+actionText: 关于我
 actionLink: /views/personalInfo/
 features:
-- title: Who am I？
+- title: 我是谁？
   details: A programmer? A human being? Or a matter combination?
-- title: Where am I？
+- title: 我在哪？
   details: In a city? On earth? Or in a small part of space?
-- title: What am I gona do？
+- title: 我将做什么？
   details: Work? Struggle? Or go to a destination I don't know?
 # footer: 'reco_luan'
 ---
